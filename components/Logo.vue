54d85a0404ec0ff08680157a5ae1a07e18c71715
Wirya Ramadhan <adhaniscuber@gmail.com>
2021-01-21 01:23:25 +0700

    initial and setup

---
+++ components/Logo.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg
+    width="240"
+    height="240"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0)">
+      <path
+        d="M7 7C7 4.23858 9.23858 2 12 2C14.7614 2 17 4.23858 17 7V8H7V7Z"
+        stroke="#33D475"
+        stroke-width="2"
+      />
+      <path
+        d="M1 8H23V15C23 19.4183 19.4183 23 15 23H9C4.58172 23 1 19.4183 1 15V8Z"
+        fill="white"
+        stroke="black"
+        stroke-width="2"
+        stroke-linejoin="round"
+      />
+      <rect x="6" y="6" width="2" height="5" rx="1" fill="#33D475" />
+      <rect x="16" y="6" width="2" height="5" rx="1" fill="#33D475" />
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">monitip</h1>
+      <h3 class="subtitle">COMING SOON</h3>
+      <!-- <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', sans-serif;
+  display: block;
+  font-weight: 600;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-family: 'Quicksand', sans-serif;
+  font-weight: 300;
+  font-size: 32px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
